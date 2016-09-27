98d0f1f33d76d87dca9e1f62f6cb8ccaf104e645
Topi99 <nokoayzack@gmail.com>
2016-09-26 20:38:21 -0500

    Tags added

--- posts/templatetags/posts_tags.py
+++ posts/templatetags/posts_tags.py
@@ -1,8 +1,14 @@
 from django import template
 from ..models import Post
+from taggit.models import Tag
 
 register = template.Library()
 
 @register.assignment_tag(takes_context=True)
 def ultimos(count=6):
 	return Post.objects.all().order_by('-fecha')[count:]
+
+@register.inclusion_tag('tags/todos_tags.html')
+def todos_tags():
+	tags = Tag.objects.all()
+	return {'tags':tags}
